7fc4f88384481a13c80f30ba09289f90e26e295b
hiddevb <hiddevanbavel@hotmail.com>
2025-11-17 12:43:18 +0100

    feature: turn of echoing

--- main.zig
+++ main.zig
@@ -1,7 +1,22 @@
 const std = @import("std");
 
+fn enableRawMode() !void {
+    const tty = try std.fs.openFileAbsolute("/dev/tty", .{ .mode = .read_write });
+    defer tty.close();
+
+    var term = try std.posix.tcgetattr(tty.handle);
+    // const orig = term;
+
+    term.lflag.ECHO = false;
+
+    try std.posix.tcsetattr(tty.handle, std.posix.TCSA.NOW, term);
+    // defer std.posix.tcsetattr(tty.handle, std.posix.TCSA.NOW, orig) catch {};
+}
+
 pub fn main() !void {
-    var buf: [256]u8 = undefined;
+    try enableRawMode();
+
+    var buf: [128]u8 = undefined;
     var reader = std.fs.File.stdin().reader(&buf);
     const stdin = &reader.interface;
 
